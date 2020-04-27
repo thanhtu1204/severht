9724c25c0607445775e0c5ff4df10129381c08c3
thanhtu1204 <tumvph05490@fpt.edu.vn>
2020-04-27 23:11:09 +0800

    anh beo

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express')
 var app = express()
-
+var indexRouter = require('./routes/index');
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-    response.send('./routes/index')
-})
+app.use('/', indexRouter);
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
