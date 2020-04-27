8c351c27538a922a560e032c8af99dd26f30653d
thanhtu1204 <tumvph05490@fpt.edu.vn>
2020-04-28 02:18:46 +0800

    anh beo

--- app.js
+++ app.js
@@ -48,5 +48,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+// Start Server
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+});
 module.exports = app;
