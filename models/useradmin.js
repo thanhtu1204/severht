168b0bf30d414e1ae273be938a327c69e65f1221
thanhtu1204 <tumvph05490@fpt.edu.vn>
2020-05-03 01:40:58 +0800

    anh beo

--- models/useradmin.js
+++ models/useradmin.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var UserAdmin = new Schema({
+var Useradmin = new Schema({
     user: {type: String, unique: true},
     password: {type: String}
 });
-module.exports = mongoose.model('userAdmin', UserAdmin);
+module.exports = mongoose.model('userAdmin', Useradmin);
